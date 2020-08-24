db36c3562e526e580defdca7545279645b0a8bef
savithamark <savithamark.093@gmail.com>
2020-08-24 09:30:51 +0530

    flaks blue print

---
+++ flask_blueprint/flask_blueprint_tutorial/__init__.py
@@ -0,0 +1,28 @@
+"""Initialize Flask app."""
+from flask import Flask
+from flask_assets import Environment  # <-- Import `Environment`
+
+
+def create_app():
+    """Create Flask application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    assets = Environment()  # Create an assets environment
+    assets.init_app(app)  # Initialize Flask-Assets
+
+    with app.app_context():
+        # Import parts of our application
+        from .profile import profile
+        from .home import home
+        from .products import products
+        from .assets import compile_static_assets  # Import logic
+
+        # Register Blueprints
+        app.register_blueprint(profile.account_bp)
+        app.register_blueprint(home.home_bp)
+        app.register_blueprint(products.product_bp)
+
+        # Compile static assets
+        compile_static_assets(assets)  # Execute logic
+
+        return app
